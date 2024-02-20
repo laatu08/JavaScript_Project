ba963fbe85d3d961a3ce18ccf0024bdb41259c65
laatu08 <143783492+laatu08@users.noreply.github.com>
2024-02-20 20:20:18 +0530

    Add files via upload

---
+++ Simple Quiz/script.js
@@ -0,0 +1,125 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answer: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "Which is largest planet in the Solar System?",
+        answer: [
+            { text: "Jupiter", correct: true },
+            { text: "Earth", correct: false },
+            { text: "Pluto", correct: false },
+            { text: "Sun", correct: false },
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answer: [
+            { text: "Sahara", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Antartica", correct: true },
+            { text: "Australia", correct: false },
+        ]
+    },
+    {
+        question: "What is 'Two Zero Two Four'?",
+        answer: [
+            { text: "2024", correct: false },
+            { text: "0024", correct: false },
+            { text: "0044", correct: true },
+            { text: "2044", correct: false },
+        ]
+    }
+]
+
+const questionElement = document.getElementById("question")
+const answerButton = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next")
+
+let currentQuestionIndex = 0
+let score = 0
+
+function startQuiz() {
+    currentQuestionIndex = 0
+    score = 0
+    nextButton.innerHTML = "Next"
+    showQuestion()
+}
+
+function showQuestion() {
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerHTML = answer.text
+        button.classList.add("btn")
+        answerButton.appendChild(button)
+
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+    })
+}
+
+function resetState() {
+    nextButton.style.display = "none"
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectBtn = e.target
+    const isCorrect = selectBtn.dataset.correct === "true"
+    if (isCorrect) {
+        selectBtn.classList.add("correct")
+        score++
+    }
+    else {
+        selectBtn.classList.add("incorrect")
+    }
+
+    Array.from(answerButton.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct")
+        }
+        button.disabled = true
+    })
+    nextButton.style.display = "block"
+}
+
+function ShowScore() {
+    resetState()
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}`
+    nextButton.innerHTML = "Play Again"
+    nextButton.style.display = "block"
+}
+
+function handleNextButton() {
+    currentQuestionIndex++
+    if (currentQuestionIndex < questions.length) {
+        showQuestion()
+    }
+    else {
+        ShowScore()
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton()
+    }
+    else {
+        startQuiz()
+    }
+})
+startQuiz()
